fd8a3b31169997e2f177c84508ee7ac1d1dc8538
Fayez Muhammed <117552186+fayezmuhammed@users.noreply.github.com>
2025-12-29 22:50:57 +0530

    updated X

--- app/landingpage/components/X.tsx
+++ app/landingpage/components/X.tsx
@@ -9,7 +9,7 @@ export default function X(props: Xprops) {
   return (
     <Image
       priority
-      src="https://firebasestorage.googleapis.com/v0/b/tedxccet.appspot.com/o/assets%2Fxlogo.png?alt=media&token=909c4bc2-d2da-43eb-addc-fee9a1501a53"
+      src="/X-hero.webp"
       height={600}
       width={700}
       alt="TEDx X Logo"
